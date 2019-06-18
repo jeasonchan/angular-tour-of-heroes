92479abe4e8657d266a6758258877c6d90659d87
jeasonchan <439994526@qq.com>
2019-06-18 23:56:51 +0800

    2019年6月17日23:06:09 将getHeroes的同步方法变为使用rxjs的observeable和of的异步方法，实现从服务器读取数据的异步调用 next：显示消息

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,16 +1,26 @@
 import {Injectable} from '@angular/core';
 import {Hero} from './hero';
 import {HEROES} from './mock-heroes';
+import {Observable, of} from 'rxjs';
 
-@Injectable({  // 这个组件是可以注入的，也就是是一个服务
-  providedIn: 'root'   // 表明可以被使用的范围
+@Injectable({  // 这个组件是可以注入的，也就是是一个服务；同理，组件也有类似的注解，这种注解内的属性及其赋值即为 元数据
+  providedIn: 'root'   // 表明可以被使用的范围，此处的范围是 root 即为根目录范围
 })
+
 export class HeroService {
 
   constructor() {
   }
 
-  getHeroes(): Hero[] {   // 定一个函数，返回Hero类型的数组
+
+  // 通常，获取数据是异步行为，这个过程是需要等待的，因此，通过angular自带的的异步获取方法来获取远端的数据
+  // 此处考虑使用HttpClient.get，该方法会返回Observable（可观察对象）
+  __getHeroes(): Hero[] {   // 定义一个函数，返回Hero类型的数组
     return HEROES;  // 返回一个常量数组
+  }
+
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES); // of(HEROES) 会返回一个 Observable<Hero[]>，它会发出单个值，这个值就是这些模拟英雄的数组。
+
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,27 +1,43 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from '../Hero';
-import {HEROES} from '../mock-heroes';;
+import {HeroService} from '../hero.service';
 
 @Component({      // @component是装饰器函数，为该组件执行angular所需要的元数据
   selector: 'app-heroes',  // 组件的选择器，css选择器，表明本组件的html元素名称是<app-heroes>，相当于<body>、<h1>之类的
   templateUrl: './heroes.component.html', // 组件模板文件位置
   styleUrls: ['./heroes.component.css']   // 组件私有css样式表文件位置
 })
+
 export class HeroesComponent implements OnInit {
 
   selectedHero: Hero; // 一个属性hero，数据/对象类型是Hero类，并对这个Hero实例进行赋值/初始化
 
-  heroes = HEROES; // import进来后，赋值后才能使用！导进来的是一个类，自己实例化后才能使用。
+  heroes: Hero[]; // import进来后，赋值后才能使用！导进来的是一个类，自己实例化后才能使用。
 
+  private heroService: HeroService;
 
-  constructor() {
+  constructor(heroService: HeroService) {
+    this.heroService = heroService;
   }
 
   ngOnInit() {
+    this.getHeroes();  // 和python一样，只要是调用类实例的的方法或者变量都需要通过this，哪怕是在类的内部
   }
 
   onSelect(everyHero: Hero) {
     this.selectedHero = everyHero;
   }
+
+  // 本方法使用老的同步方法
+  __getHeroes(): void {   //  职责分离，让service去获取数据，而不是前端的组件
+    this.heroes = this.heroService.__getHeroes();
+  }
+
+  // 对老方法进行改进，使用observeable<>泛型包装传输的Hero[]对象,达到异步调用的目的
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(ObservableObjectWeGot => this.heroes = ObservableObjectWeGot);
+    // subscribe()中使用了回调函数，也是lambda表达式
+  }
+
 
 }
