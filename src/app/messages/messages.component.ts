7a4ca9e7b6db6485aee41d21325a2ef3e0d0ab8e
jeasonchan <439994526@qq.com>
2019-06-22 22:52:33 +0800

    2019年6月22日22:52:27

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 
 // todo 将messageService导入此模块，并注入服务，以从中读取要显示的数据。
+import {MessageService} from '../message.service';
 
 
 @Component({
@@ -10,7 +11,10 @@ import {Component, OnInit} from '@angular/core';
 })
 export class MessagesComponent implements OnInit {
 
-  constructor() {
+  private messageService: MessageService; // 模板只能绑定到组件的公共属性！！！！！！
+
+  constructor(messageService: MessageService) {
+    this.messageService = messageService; // 注入服务，从服务中心服务读取数据，以让模板文件展示给用户
   }
 
   ngOnInit() {
